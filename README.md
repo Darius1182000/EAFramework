10b2d3ed673ae174836cd42453ac0d233b6e1e83
Darius1182000 <merca_darius@yahoo.ro>
2024-02-19 10:34:17 +0200

    Readme update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # EAFramework
-Selenium Framwork
+Selenium Framework
+
+Drivers: Mozilla Firefox - geckodriver [v0.34.0](https://github.com/mozilla/geckodriver/releases)
+
--- src/test/com/ea/test/Features/SeleniumTest/LoginTest.java
+++ src/test/com/ea/test/Features/SeleniumTest/LoginTest.java
@@ -1,4 +1,32 @@
 package com.ea.test.Features.SeleniumTest;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
 public class LoginTest {
+
+    //Global variables
+    private WebDriver driver = null;
+    @BeforeEach
+    public void Initialize(){
+        //Open the browser
+        System.setProperty("webdriver.gecko.driver", "D:\\SeleniumDrivers\\geckodriver-v0.33.0.exe");
+
+        driver = new FirefoxDriver();
+        driver.navigate().to("http://localhost:3000/signin");
+
+    }
+    @Test
+    public void Login(){
+
+        driver.findElement(By.name("username")).sendKeys("test");
+        driver.findElement(By.name("password")).sendKeys("test");
+        driver.findElement(By.xpath("//button[@data-test='signin-submit']")).submit();
+
+    }
 }
