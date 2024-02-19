d30e4c7e39d4e9c35c87227389dc28579b614b0c
fortnight <55536924+Darius1182000@users.noreply.github.com>
2024-02-19 11:47:59 +0200

    buildPOMlogic (#2)

--- src/test/com/ea/test/Features/Pages/LoginPage.java
+++ src/test/com/ea/test/Features/Pages/LoginPage.java
@@ -1,4 +1,31 @@
 package com.ea.test.Features.Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+
+    //Initialize the PageFactory
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy (how = How.NAME, using = "username")
+    public WebElement usernameField;
+
+    @FindBy (how = How.NAME, using = "password")
+    public WebElement passwordField;
+
+    @FindBy (how = How.XPATH, using = "//button[@data-test='signin-submit']")
+    public WebElement loginButton;
+
+    public void Login(String Username, String Password){
+
+        usernameField.sendKeys(Username);
+        passwordField.sendKeys(Password);
+        loginButton.submit();
+    }
 }
