9f83b158126eb5334bc919da5e2be61d55f8b1d9
Merca Darius <darius.merca@fanduel.com>
2024-02-21 10:10:11 +0200

    mac Working

--- src/test/com/ea/test/Features/Pages/LoginPage.java
+++ src/test/com/ea/test/Features/Pages/LoginPage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage extends BasePage {
 
@@ -18,16 +17,29 @@ public class LoginPage extends BasePage {
     @FindBy (how = How.NAME, using = "username")
     public WebElement usernameField;
 
+    @FindBy (how = How.NAME, using = "loginname")
+    public WebElement loginNameField;
+
     @FindBy (how = How.NAME, using = "password")
     public WebElement passwordField;
 
     @FindBy (how = How.XPATH, using = "//button[@data-test='signin-submit']")
     public WebElement loginButton;
 
+    @FindBy (how = How.XPATH, using = "//button[@title='Login']")
+    public WebElement signInButton;
+
     public void Login(String Username, String Password){
 
         usernameField.sendKeys(Username);
         passwordField.sendKeys(Password);
         loginButton.submit();
+    }
+
+    public void SignIn(String LoginName, String Password){
+
+        loginNameField.sendKeys(LoginName);
+        passwordField.sendKeys(Password);
+        signInButton.submit();
     }
 }
--- src/test/com/ea/test/Features/SeleniumTest/LoginTest.java
+++ src/test/com/ea/test/Features/SeleniumTest/LoginTest.java
@@ -1,13 +1,10 @@
 package com.ea.test.Features.SeleniumTest;
 
 import com.ea.test.Features.Pages.LoginPage;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class LoginTest {
 
@@ -15,27 +12,23 @@ public class LoginTest {
     private WebDriver driver = null;
     @BeforeEach
     public void Initialize(){
-        //Open the browser
         //Windows
         //System.setProperty("webdriver.gecko.driver", "D:\\SeleniumDrivers\\geckodriver_mac-v0.33.0.exe");
         //MacOS
-        System.setProperty("webdriver.gecko.driver", "lib/geckodriver3_mac");
+        System.setProperty("webdriver.gecko.driver", "lib/geckodriverMAC");
 
         driver = new FirefoxDriver();
         //driver.navigate().to("http://localhost:3000/signin");
-        driver.navigate().to("https://www.google.com/");
+        driver.navigate().to("https://automationteststore.com/index.php?rt=account/login");
 
     }
     @Test
     public void Login(){
 
-//        driver.findElement(By.name("username")).sendKeys("test");
-//        driver.findElement(By.name("password")).sendKeys("test12345");
-//        driver.findElement(By.xpath("//button[@data-test='signin-submit']")).submit();
-        //test
-
+//        LoginPage page = new LoginPage(driver);
+//        page.Login("nume", "parola");
         LoginPage page = new LoginPage(driver);
-        page.Login("nume", "parola");
+        page.SignIn("darius", "parola1");
 
     }
 }
